ec1bed72ecadea1f4897105a706bf9b6a3adcc71
zhyravlevdima <zhyravlevdima@yandex.com>
2022-09-13 22:11:50 +0300

    Task1_1

--- Lab_1/Task_1/Task_1_1/Task_1_1.cpp
+++ Lab_1/Task_1/Task_1_1/Task_1_1.cpp
@@ -1,6 +1,7 @@
 #include "Task_1_1.h"
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 /*	Составить программу для расчёта двух значений z1 и z2, результаты которых должны совпадать. 
 	Значения исходных данных вводить с клавиатуры, pi = 3.1415926 , a и b - в радианах 
@@ -12,11 +13,20 @@ const double pi =  3.1415926;
 void main(void) {
 
 	double z1, z2;
-	double angleRadian = 4;
+	double angleRadian;
+
+	printf("angle in radian = ");
+	scanf_s("%lf", &angleRadian);
+
 	double angleGrade = 180 / pi * angleRadian;
+
 
 	z1 = (sqrt(pow(3 * angleGrade + 2,2) - 24 * angleGrade)) / (3 * sqrt(angleGrade) - 2 / sqrt(angleGrade));
 	z2 = -sqrt(angleGrade);
 
-	printf("Answer:\n\tz1 = %f\n\tz2 = %f", z1, z2);
+	printf("Answer:\n\tz1 = %f\n\tz2 = %f\n", z1, z2);
+	
+	system("pause");
+
+	
 }
