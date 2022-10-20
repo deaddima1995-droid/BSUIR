2215ef7f24307e960575452489261c06dbcb49f2
Дмитрий Журавлёв <zhyravlevdima@yandex.com>
2022-10-20 22:24:53 +0300

    added task_5_2

---
+++ Lab_1/Task_5/Task_5_2/Task_5_2.cpp
@@ -0,0 +1,45 @@
+﻿/*
+Написать программу по обработке двухмерного динамического массива. 
+Размеры массива n, m и значения его элементов вводятся с клавиатуры.
+В квадратной матрице найти максимальный среди элементов, лежащих ниже побочной диагонали.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int lines;
+    int max = INT32_MIN;
+
+    cout << "Print size of matrix:";
+    cin >> lines;
+    
+    int** matrix = (int**)malloc(lines * sizeof(int*));
+    for (int i = 0; i < lines; i++) {
+        matrix[i] = (int*)malloc(lines * sizeof(int));
+    }
+
+    // Add number to array
+    printf("Print your numbers from space matrix[%d][%d]\n", lines, lines);
+    for (int i = 0; i < lines; i++) {
+        for (int y = 0; y < lines; y++) {
+            cin >> matrix[i][y];
+        }
+    }
+    cout << endl;
+    // Print array
+    for (int i = 0; i < lines; i++) {
+        for (int y = 0; y < lines; y++) {
+            cout << matrix[i][y] << "\t";
+        }
+        cout << endl;
+    }
+    
+    // Search max
+    for (int i = lines - 1, y = 0; i > 0; i--, y++) {
+        max = (matrix[i][y] > max) ? matrix[i][y] : max;
+    }
+    cout << "Max value:" << max << endl;
+}
+
