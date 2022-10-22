038d5a2e5c9c0eb144467c08fa37375f936255b0
Дмитрий Журавлёв <zhyravlevdima@yandex.com>
2022-10-22 14:48:37 +0300

    added Task_6_2

--- Lab_1/Task_6/Task_6_2/Task_6_2.cpp
+++ Lab_1/Task_6/Task_6_2/Task_6_2.cpp
@@ -8,9 +8,25 @@
 using namespace std;
 
 int main() {
-    int k;
-
+    int splitter, nextWord;
+    string outFirst, outSecond;
     string text = "Hello world hello world";
-    text[0] = 'c';
-    cout << text;
+    
+    do {
+        cout << "Write number of symbol:";
+        cin >> splitter;
+    } while (splitter < 1 || splitter > (int) text.length());
+
+    for (int i = splitter; i < (int) text.length(); i++) {
+        if (text[i] == ' ') {
+            splitter = i;
+            break;
+        }
+    }
+
+    outFirst.append(text, 0, splitter);
+    outSecond.append(text, splitter + 1, text.length() - splitter);
+    cout << text << endl;
+    cout << outFirst << endl;
+    cout << outSecond << endl;
 }
