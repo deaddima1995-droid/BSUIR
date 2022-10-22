6cf185f17f00cfc8275959dd5f0fa3d8f903f3a9
zhyravlevdima <zhyravlevdima@yandex.com>
2022-10-22 16:17:01 +0300

    update Task_6_2

--- Lab_1/Task_6/Task_6_2/Task_6_2.cpp
+++ Lab_1/Task_6/Task_6_2/Task_6_2.cpp
@@ -9,23 +9,36 @@ using namespace std;
 
 int main() {
     int splitter, nextWord;
+    string text = "My brothers are very smart and work hard in school";
     string outFirst, outSecond;
-    string text = "Hello world hello world";
     
     do {
         cout << "Write number of symbol:";
         cin >> splitter;
     } while (splitter < 1 || splitter > (int) text.length());
 
-    for (int i = splitter; i < (int) text.length(); i++) {
+    for (int i = splitter; i >= 0; i--) {
         if (text[i] == ' ') {
             splitter = i;
             break;
+        } else if (i == 0) {
+            splitter = 0;
         }
     }
 
+    if (text[splitter] == ' ') {
+      for (int i = splitter; i < (int) text.length(); i++) {
+          if (text[i] != ' ') {
+              nextWord = i;
+              break;
+        }
+      }
+    } else {
+        nextWord = splitter;
+    }
+
     outFirst.append(text, 0, splitter);
-    outSecond.append(text, splitter + 1, text.length() - splitter);
+    outSecond.append(text, nextWord, text.length() - splitter);
     cout << text << endl;
     cout << outFirst << endl;
     cout << outSecond << endl;
