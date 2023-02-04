301582fbb60a46fa4a3ff36c01b1bd6b3c17fb77
Дмитрий Журавлёв <zhyravlevdima@yandex.com>
2023-02-04 15:13:32 +0300

    Task_1 added

--- Lab_2/Task_1/Task_1.cpp
+++ Lab_2/Task_1/Task_1.cpp
@@ -1,5 +1,5 @@
 ﻿/*
-13. Вычислить y = xn по следующему правилу: 
+13. Вычислить y = x^n по следующему правилу: 
 y = ( x^(n/2) )2, если n четное, 
 и 
 y = x * y^(n-1), если n нечетное.
@@ -14,16 +14,15 @@ using namespace std;
 int degree(int x, int n);
 
 int main() {
-    cout << "Hello World!\n";
+    cout << "Answer: " << degree(2,10);
 }
 
 int degree(int x, int n) {
     if (n == 0) return 1;
+    if (n == 1) return x;
 
-    if (n % 2 == 0 || n != 0) {
-        return (pow(pow(x, n / 2), 2));
-    } else {
-
+    if (n % 2 == 0) {
+        return pow(degree(x, n / 2), 2);
     }
-    return 0;
+    return x * degree(x, n - 1);
 }
