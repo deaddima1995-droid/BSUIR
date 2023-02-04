84a79774278e5168746cc9f5c39de29426b4e5fa
zhyravlevdima <zhyravlevdima@yandex.com>
2023-02-04 14:07:09 +0300

    Lab_2

--- Lab_2/Task_1/Task_1.cpp
+++ Lab_2/Task_1/Task_1.cpp
@@ -1,8 +1,29 @@
-﻿
+﻿/*
+13. Вычислить y = xn по следующему правилу: 
+y = ( x^(n/2) )2, если n четное, 
+и 
+y = x * y^(n-1), если n нечетное.
+*/
+
+
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+int degree(int x, int n);
+
 int main() {
     cout << "Hello World!\n";
+}
+
+int degree(int x, int n) {
+    if (n == 0) return 1;
+
+    if (n % 2 == 0 || n != 0) {
+        return (pow(pow(x, n / 2), 2));
+    } else {
+
+    }
+    return 0;
 }
