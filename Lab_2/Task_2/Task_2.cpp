94df3157525b201c076d3069152e2ec0e4392024
zhyravlevdima <zhyravlevdima@yandex.com>
2023-02-05 11:11:35 +0300

    update

--- Lab_2/Task_2/Task_2.cpp
+++ Lab_2/Task_2/Task_2.cpp
@@ -1,8 +1,8 @@
 ﻿/*
 13. В кассе хранится информация о поездах на ближайшую неделю: 
-- дата выезда, 
-- пункт назначения, 
-- время отправления, 
+- дата выезда, +
+- пункт назначения, +
+- время отправления, +
 - число свободных мест. 
 Необходимо зарезервировать m мест до города N на k-й день недели с временем отправления поезда не позднее t часов. 
 Вывести время отправления или сообщение о невозможности выполнить заказ. Ключ: число свободных мест.
@@ -11,10 +11,33 @@
 
 using namespace std;
 
-struct train {
-
+struct Time {
+    Time() {}
+    Time(int hour, int minute, int second) {
+        this->hour = hour;
+        this->minute = minute;
+        this->second = second;
+    }
+    int hour, minute, second;
 };
+
+struct Date {
+    int day, month, year;
+};
+
+struct Train {
+    Time departureTime{};
+    Date departureDate{};
+    char destination[15];
+    uint32_t freeSpace{};
+};
+
+
+
+
+
 
 int main() {
     cout << "Hello World!\n";
 }
+
