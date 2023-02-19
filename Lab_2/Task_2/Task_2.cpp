9e2614729522b5b26554afdc5f081401749f85b2
zhyravlevdima <zhyravlevdima@yandex.com>
2023-02-19 17:19:30 +0300

    update

--- Lab_2/Task_2/Task_2.cpp
+++ Lab_2/Task_2/Task_2.cpp
@@ -18,18 +18,20 @@
 #include <fstream>
 #include <time.h>
 
-#define LENGHT_CHAR_ARRAY 10
+#define LENGHT_CHAR_ARRAY 12
+#define COUNT_DAYS 7
+#define COUNT_CITIES 5
 
 using namespace std;
 
 int GetRandomNumber(int min, int max);
-void createFile();
-void readFile();
-void addTrainToFile();
+void createFile(char* name);
+void readFile(char* name);
+void addTrainToFile(char *name);
 
 
-char daysOfWeek[7][LENGHT_CHAR_ARRAY] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thirsday", "Friday", "Saturday" };
-char cities[5][LENGHT_CHAR_ARRAY] = { "Misnk", "Mogilev", "Brest", "Vitebsk", "Grodno" };
+char daysOfWeek[COUNT_DAYS][LENGHT_CHAR_ARRAY] = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+char cities[COUNT_CITIES][LENGHT_CHAR_ARRAY] = { "Минск", "Могилёв", "Брест", "Витебск", "Гродно" };
 
 
 
@@ -47,20 +49,19 @@ struct Time {
 
 struct Train {
     Train() {
-        departureTime.hour = 0;
-        departureTime.minute = 0;
-        freeSpace = 0;
+        departureTime = Time(0, 0);
     }
     Train(bool) {
         departureTime = Time();
-        strcpy_s(departureDate, daysOfWeek[GetRandomNumber(0, 6)]);
-        strcpy_s(destination, cities[GetRandomNumber(0, 4)]);
+        strcpy_s(departureDate, daysOfWeek[GetRandomNumber(0, COUNT_DAYS - 1)]);
+        strcpy_s(destination, cities[GetRandomNumber(0, COUNT_CITIES - 1)]);
         freeSpace = GetRandomNumber(0, 20);
-        }
+    }
+
     Time departureTime;
     char departureDate[LENGHT_CHAR_ARRAY]{};
     char destination[LENGHT_CHAR_ARRAY]{};
-    int freeSpace;
+    int freeSpace{};
 };
 
 
@@ -79,19 +80,23 @@ ostream& operator << (ostream& out, Train *train) {
 
 
 int main() {
+    
     setlocale(LC_ALL, "ru");
     srand(time(NULL));
-    
-    createFile();
-    readFile();
+    char* name = new char;
+    cin >> name;
+    createFile(name);
+    readFile(name);
+    addTrainToFile(name);
+    readFile(name);
     
 }
 
-void createFile(){ 
-    struct  Train train = Train(true);
+void createFile(char* name){
+    struct Train train = Train(true);
     int counter = 0;
 
-    fstream outTrain("test", std::fstream::out | std::fstream::binary);
+    fstream outTrain(name, std::fstream::out | std::fstream::binary);
     if (!outTrain) {
         cerr << "Файл не создан\n";
         return;
@@ -106,24 +111,44 @@ void createFile(){
     outTrain.close();
 }
 
-void readFile() {
-
-    fstream inTrain("test", std::fstream::in | std::fstream::binary);
+void readFile(char* name) {
+    fstream inTrain(name, std::fstream::in | std::fstream::binary);
     if (!inTrain) {
-        cout << "Нельзя открыть файл для чтения\n";
+        cerr << "Нельзя открыть файл для чтения\n";
+        return;
     }
     Train *rTrain = new Train();
     while (inTrain.read((char*)rTrain, sizeof(struct Train))) {
         cout << rTrain;
-        //cout << rTrain->destination << endl << rTrain->departureDate << endl << rTrain->departureTime << endl;
         cout << endl;
     }
-
+    delete rTrain;
     inTrain.close();
 }
 
-void addTrainToFile() {
+void addTrainToFile(char* name) {
+    fstream addTrain(name, std::fstream::out | std::fstream::app |std::fstream::binary);
+    if (!addTrain) {
+        cout << "Нельзя открыть файл для чтения\n";
+        return;
+    }
+    auto train = new Train();
+    char* temp = new char;
+    cout << "Введите пункт назначения: ";
+    cin >> temp;
+    strcpy_s(train->destination, temp);
+    cout << "Введите день отправления:";
+    cin >> temp;
+    strcpy_s(train->departureDate, temp);
+    cout << "Введите время отправления";
+    cin >> train->departureTime.hour;
+    cin >> train->departureTime.minute;
+    cout << "Введите количество свободных мест";
+    cin >> train->freeSpace;
 
+    addTrain.write((char*)train, sizeof(struct Train));
+    addTrain.close();
+    cout << "Добавлен поезд" << train << endl << endl;
 }
 
 int GetRandomNumber(int min, int max) {
