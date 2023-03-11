b86dd2fa746114063a67b7b89ebd31fd21e5c09d
zhyravlevdima <zhyravlevdima@yandex.com>
2023-03-11 20:50:58 +0300

    update

--- Lab_2/Task_2/Task_2.cpp
+++ Lab_2/Task_2/Task_2.cpp
@@ -13,6 +13,28 @@
         - число свободных мест. 
         Необходимо зарезервировать m мест до города N на k-й день недели с временем отправления поезда не позднее t часов. 
         Вывести время отправления или сообщение о невозможности выполнить заказ. Ключ: число свободных мест.
+
+        Двоичный поиск
+int binarySearch(int[] arr, int key) {
+    int low = 0;
+    int high = arr.length - 1;
+
+    while (low <= high) {
+        int mid = (low + high) >>> 1;
+        int midVal = arr[mid];
+
+        if (midVal < key)
+            low = mid + 1;
+        else if (midVal > key)
+            high = mid - 1;
+        else
+            return mid; // key found
+    }
+    return -(low + 1);  // key not found.
+}
+
+
+
 */
 
 #include <iostream>
@@ -22,41 +44,37 @@
 #define LENGHT_CHAR_ARRAY 12
 #define COUNT_DAYS 7
 #define COUNT_CITIES 5
+#define COUNT_FREE_SPACE 20
 
 using namespace std;
-
-int GetRandomNumber(int min, int max);
-void createFile(char* name);
-void readFile(char* name);
-void addTrainToFile(char *name);
-
 
 char daysOfWeek[COUNT_DAYS][LENGHT_CHAR_ARRAY] = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
 char cities[COUNT_CITIES][LENGHT_CHAR_ARRAY] = { "Минск", "Могилёв", "Брест", "Витебск", "Гродно" };
 
 
-
 struct Time {
     Time() {
-        this->hour = GetRandomNumber(0, 23);
-        this->minute = GetRandomNumber(0, 59);
+        this->hour = 0;
+        this->minute = 0;
     }
     Time(int hour, int minute) {
         this->hour = hour;
         this->minute = minute;
     }
-    int hour{}, minute{};
+    int hour, minute;
 };
 
 struct Train {
     Train() {
-        departureTime = Time(0, 0);
+        this->departureTime = Time();
+        this->freeSpace = 0;
     }
-    Train(bool) {
-        departureTime = Time();
-        strcpy_s(departureDate, daysOfWeek[GetRandomNumber(0, COUNT_DAYS - 1)]);
-        strcpy_s(destination, cities[GetRandomNumber(0, COUNT_CITIES - 1)]);
-        freeSpace = GetRandomNumber(0, 20);
+    Train(int hour, int minute, int date, int destiny, int freeSpace) {
+        this->departureTime.hour = hour;
+        this->departureTime.minute = minute;
+        strcpy_s(this->departureDate, daysOfWeek[date]);
+        strcpy_s(this->destination, cities[destiny]);
+        this->freeSpace = freeSpace;
     }
 
     Time departureTime;
@@ -79,6 +97,13 @@ ostream& operator << (ostream& out, Train *train) {
     return out;
 }
 
+void createFile(char* name);
+void readFile(char* name);
+void addTrainToFile(char* name);
+bool linearSerach(Train *tTemp, int key);
+int getRandomNumber(int min, int max);
+Train* getRandomTrain();
+
 
 int main() {
     system("chcp 1251");
@@ -86,15 +111,15 @@ int main() {
     srand(time(NULL));
     char* name = new char;
     cin >> name;
-    //createFile(name);
-    //readFile(name);
+    createFile(name);
+    readFile(name);
     addTrainToFile(name);
     readFile(name);
     
 }
 
 void createFile(char* name){
-    struct Train train = Train(true);
+    Train* train = new Train();
     int counter = 0;
 
     fstream outTrain(name, std::fstream::out | std::fstream::binary);
@@ -106,8 +131,8 @@ void createFile(char* name){
     cin >> counter;
 
     for (int i = 0; i < counter; i++) {
-        train = Train(true);
-        outTrain.write((char*)&train, sizeof(struct Train));
+        train = getRandomTrain();
+        outTrain.write((char*)train, sizeof(struct Train));
     }
     outTrain.close();
 }
@@ -136,10 +161,8 @@ void addTrainToFile(char* name) {
     auto train = new Train();
     cout << "Введите пункт назначения: ";
     cin >> train->destination;
-    //strcpy_s(train->destination, temp);
     cout << "Введите день отправления:";
     cin >> train->departureDate;
-    //strcpy_s(train->departureDate, temp);
     cout << "Введите время отправления";
     cin >> train->departureTime.hour;
     cin >> train->departureTime.minute;
@@ -151,6 +174,21 @@ void addTrainToFile(char* name) {
     cout << "Добавлен поезд" << train << endl << endl;
 }
 
-int GetRandomNumber(int min, int max) {
+bool linearSerach(Train* tTemp, int key) {
+    if (tTemp->freeSpace == key) {
+        return true;
+    }
+    return false;
+}
+
+int getRandomNumber(int min, int max) {
     return min + rand() % (max - min + 1);
+}
+
+Train* getRandomTrain() {
+    return new Train(getRandomNumber(0, 23),
+                     getRandomNumber(0, 59),
+                     getRandomNumber(0,COUNT_DAYS - 1),
+                     getRandomNumber(0, COUNT_CITIES - 1),
+                     getRandomNumber(0, COUNT_FREE_SPACE));
 }
