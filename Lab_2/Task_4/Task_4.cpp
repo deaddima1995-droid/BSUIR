1b4bdf5cfc89c1af5a0538fd222e98d14371bfc2
Дмитрий Журавлёв <zhyravlevdima@yandex.com>
2023-05-01 22:04:11 +0300

    update

--- Lab_2/Task_4/Task_4.cpp
+++ Lab_2/Task_4/Task_4.cpp
@@ -1,5 +1,5 @@
-/*РќР°РїРёСЃР°С‚СЊ РїСЂРѕРіСЂР°РјРјСѓ РїРѕ СЃРѕР·РґР°РЅРёСЋ, РґРѕР±Р°РІР»РµРЅРёСЋ (РІ РЅР°С‡Р°Р»Рѕ, РІ РєРѕРЅРµС†),
-  РїСЂРѕСЃРјРѕС‚СЂСѓ (СЃ РЅР°С‡Р°Р»Р°, СЃ РєРѕРЅС†Р°) Рё СЂРµС€РµРЅРёСЋ РїСЂРёРІРµРґРµРЅРЅРѕР№ РІ РїРѕРґСЂР°Р·РґРµР»Рµ 3.3 Р·Р°РґР°С‡Рё РґР»СЏ РґРІСѓРЅР°РїСЂР°РІР»РµРЅРЅС‹С… Р»РёРЅРµР№РЅС‹С… СЃРїРёСЃРєРѕРІ.
+/*Написать программу по созданию, добавлению (в начало, в конец),
+  просмотру (с начала, с конца) и решению приведенной в подразделе 3.3 задачи для двунаправленных линейных списков.
  */
 
 #include <iostream>
@@ -24,6 +24,9 @@ void addInFront(Queue *queue, int value);
 void printFromFront(Queue *queue);
 void printFromEnd(Queue *queue);
 void consolePause();
+void delAll(Queue &queue);
+int getRandomNumber(int min, int max);
+
 
 int main() {
     srand(time(nullptr));
@@ -31,14 +34,61 @@ int main() {
     system("cls");
 
     auto *queue = new Queue();
-    addInFront(queue,10);
-    addInFront(queue,9);
-    addInFront(queue,8);
-    addInEnd(queue, 11);
-
-    printFromFront(queue);
-    printFromEnd(queue);
-    consolePause();
+    int menu{};
+    // Menu
+    while(true) {
+        cout << "1. Создать\n" <<
+                "2. Добавить в начало\n" <<
+                "3. Добавить в конец\n" <<
+                "4. Просмотр с начала\n" <<
+                "5. Просмотр с конца\n" <<
+                "6. Удалить\n" <<
+                "7. Удалить максимальный элемент\n" <<
+                "0. Выход\n";
+        cin >> menu;
+        switch (menu) {
+            case 1: {
+                if (queue) {
+                    delAll(*queue);
+                }
+                queue = nullptr;
+                int iter = getRandomNumber(0,20);
+                for (int i = 0; i < iter; ++i) {
+                    addInFront(queue, getRandomNumber(-15,15));
+                }
+                system("cls");
+                break;
+            }
+            case 2: {
+                int value;
+                cout << "Введите значение:";
+                cin >> value;
+                addInFront(queue, value);
+                break;
+            }
+            case 3: {
+                int value;
+                cout << "Введите значение:";
+                cin >> value;
+                addInEnd(queue, value);
+                break;
+            }
+            case 4: {
+                printFromFront(queue);
+                break;
+            }
+            case 5: {
+                printFromEnd(queue);
+                break;
+            }
+            case 6: {
+                
+            }
+            default: {
+                return 1;
+            }
+        }
+    }
 }
 
 void addInFront(Queue *queue, int value) {
@@ -88,4 +138,18 @@ void printFromEnd(Queue *queue) {
 void consolePause() {
     system("pause");
     cin.get();
+}
+
+void delAll(Queue &queue) {
+    Node* current = queue.front;
+    while (current != nullptr) {
+        Node *temp = current;
+        current = current->next;
+        delete temp;
+    }
+    queue.front = queue.end = nullptr;
+}
+
+int getRandomNumber(int min, int max) {
+    return min + rand() % (max - min + 1);
 }
